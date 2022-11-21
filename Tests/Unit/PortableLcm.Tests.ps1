3bee3b2cfdb5886ac433842deb17f6963a904d40
Anthony Romano <aromano@scmpeoc3t.army.mil>
2022-11-21 11:05:28 -0500

    testing build

--- Tests/Unit/PortableLcm.Tests.ps1
+++ Tests/Unit/PortableLcm.Tests.ps1
@@ -1,130 +1,26 @@
 
 Import-Module "$PSScriptRoot\..\..\PortableLcm.psm1" -Force
 
-Describe 'Test-Parameter' {
-    function Test-Function
-    {
-        [cmdletbinding()]
-        param
-        (
-            [Parameter()]
-            [string]
-            $Key,
-
-            [Parameter()]
-            [string]
-            $ValueName
-        )
-
-    }
-
-    Context 'Calling Test-Parameter with no extra parameters' {
-
-        $ContextParams = @{
-            Name   = 'Test-Function'
-            Values = @{
-                Key       = 'TestRegistryKey'
-                ValueName = ''
-            }
-        }
-
-        $params = Test-Parameter @ContextParams
-
-        It 'Should return 2 params' {
-            $params.count | should be 2
-        }
-    }
-
-    Context 'Calling Test-Parameter with extra parameters' {
-
-        $ContextParams = @{
-            Name   = 'Test-Function'
-            Values = @{
-                Key       = 'TestRegistryKey'
-                ValueName = ''
-                Extra     = ''
-            }
-        }
-
-        $params = & 'Test-Parameter' @ContextParams
-
-        It 'Should return 2 params' {
-            $params.Count | should be 2
-        }
+Describe 'Assert-MofConfig' {
+    Context 'Calling Assert-MofConfig with bad path' {
+        It 'Should throw' {
+            { Assert-MofConfig -Path fake } | Should -Throw
+        }        
     }
 }
 
-Describe 'Assert-Validation' {
-    $Parameter = [System.Management.Automation.ParameterMetadata]::new('Name', 'string')
-    $Parameter.Attributes.Add([System.Management.Automation.ValidateLengthAttribute]::new(1, 3))
-
-    Context 'Calling Assert-Validation with value not meeting parameter requirements' {
-        $ContextParams = @{
-            element           = 'Test'
-            ParameterMetadata = $Parameter
-        }
-
+Describe 'Get-MofResources' {
+    Context 'Calling Get-MofResources with bad path' {
         It 'Should throw' {
-            { Assert-Validation @ContextParams } | should throw
-        }
-    }
-
-    Context 'Calling Assert-Validation with value meeting parameter requirements' {
-
-        $ContextParams = @{
-            element           = 'Yes'
-            ParameterMetadata = $Parameter
-        }
-
-        It 'Should not throw' {
-            { Assert-Validation @ContextParams } | should not throw
-        }
+            { Test-MofConfig -Path fake } | Should -Throw
+        }        
     }
 }
 
-Describe 'Test-MandatoryParameter' {
-    function Test-Function
-    {
-        [cmdletbinding()]
-        param
-        (
-            [parameter(Mandatory = $true)]
-            [string]
-            $Key,
-
-            [parameter(Mandatory = $true)]
-            [string]
-            $ValueName
-        )
-
-    }
-
-    Context 'Calling Test-MandatoryParameter with missing required parameter' {
-
-        $ContextParams = @{
-            Name   = 'Test-Function'
-            Values = @{
-                Key = 'TestRegistryKey'
-            }
-        }
-
+Describe 'Test-MofConfig' {
+    Context 'Calling Test-MofConfig with bad path' {
         It 'Should throw' {
-            { Test-MandatoryParameter @ContextParams } | should throw
-        }
-    }
-
-    Context 'Calling Test-MandatoryParameter with value meeting parameter requirements' {
-
-        $ContextParams = @{
-            Name   = 'Test-Function'
-            Values = @{
-                Key       = 'TestRegistryKey'
-                ValueName = 'Test'
-            }
-        }
-
-        It 'Should not throw' {
-            { Test-MandatoryParameter @ContextParams } | should not throw
-        }
+            { Test-MofConfig -Path fake } | Should -Throw
+        }        
     }
 }
